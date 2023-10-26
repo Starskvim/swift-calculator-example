58548d408322116a3d89db8e5b4bf28782a23b64
starskvim <starskvim@mail.ru>
2023-10-26 14:26:45 +0300

    initial

--- swift-calculator-example/swift_calculator_exampleApp.swift
+++ swift-calculator-example/swift_calculator_exampleApp.swift
@@ -40,5 +40,6 @@ class swift_calculator_exampleApp: UIResponder, UIApplicationDelegate {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
   }
 
+    
 
 }

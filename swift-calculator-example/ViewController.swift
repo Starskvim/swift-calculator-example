4c705a29fef56a33b7969c8a896541822d25fa29
starskvim <starskvim@mail.ru>
2023-10-26 14:23:32 +0300

    Initial Commit

---
+++ swift-calculator-example/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  swift-calculator-example
+//
+//  Created by Михаил Гайволя on 26.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        let number = sender.currentTitle
+        print(number as Any)
+    }
+}
+
+#Preview {
+    ViewController()
+}
